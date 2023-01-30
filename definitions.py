6ca3f6268d5f7a065215d9541a2253f138585aa5
MAFIAYONTOP <91059034+MAFIAYONTOP@users.noreply.github.com>
2023-01-30 15:10:46 +0530

    Add files via upload

---
+++ definitions.py
@@ -0,0 +1,23 @@
+import __main__
+
+#from helpers.io import load_json
+
+##CONFIG = load_json("config.json")
+
+# config constants
+user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36"
+max_vcc_uses = "4"
+concurrent_workers = "1"
+vcc_replace_timeout = "0"
+post_redeem_timeout = "0"
+authenticate_timeout = "10"
+proxyless = "true"
+
+
+# fixed constants
+RETRY_THRESHOLD = 3
+DEBUG_MODE = False
+
+ROOT_PATH = "."
+STRIPE_API_KEY = "pk_live_CUQtlpQUF0vufWpnpUmQvcdi"
+X_SUPER_PROPERTIES = "eyJvcyI6IldpbmRvd3MiLCJicm93c2VyIjoiQ2hyb21lIiwiZGV2aWNlIjoiIiwic3lzdGVtX2xvY2FsZSI6ImVuLUdCIiwiYnJvd3Nlcl91c2VyX2FnZW50IjoiTW96aWxsYS81LjAgKFdpbmRvd3MgTlQgMTAuMDsgV2luNjQ7IHg2NCkgQXBwbGVXZWJLaXQvNTM3LjM2IChLSFRNTCwgbGlrZSBHZWNrbykgQ2hyb21lLzk1LjAuNDYzOC41NCBTYWZhcmkvNTM3LjM2IiwiYnJvd3Nlcl92ZXJzaW9uIjoiOTUuMC40NjM4LjU0Iiwib3NfdmVyc2lvbiI6IjEwIiwicmVmZXJyZXIiOiIiLCJyZWZlcnJpbmdfZG9tYWluIjoiIiwicmVmZXJyZXJfY3VycmVudCI6IiIsInJlZmVycmluZ19kb21haW5fY3VycmVudCI6IiIsInJlbGVhc2VfY2hhbm5lbCI6InN0YWJsZSIsImNsaWVudF9idWlsZF9udW1iZXIiOjEwMjExMywiY2xpZW50X2V2ZW50X3NvdXJjZSI6bnVsbH0="
